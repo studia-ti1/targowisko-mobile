7c714d2831ae7cba6bcfebd1345991ad60f8c8c6
gonuit <kamilklyta341@gmail.com>
2019-10-23 14:35:12 +0100

    Add icon prop to primary button

--- lib/widgets/buttons/primary_button/primary_button.dart
+++ lib/widgets/buttons/primary_button/primary_button.dart
@@ -6,9 +6,11 @@ class PrimaryButton extends StatelessWidget {
   final String label;
   final VoidCallback onPressed;
   final bool loading;
+  final Widget icon;
 
   PrimaryButton({
     this.color,
+    this.icon,
     @required this.label,
     @required this.onPressed,
     this.loading,
@@ -16,27 +18,40 @@ class PrimaryButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(50),
-      child: Container(
-        width: MediaQuery.of(context).size.width,
-        height: 50,
-        decoration: BoxDecoration(
-          color: color ?? StyleProvider.of(context).appColors.primaryAccent,
-        ),
-        child: RawMaterialButton(
-          child: loading
-              ? CircularProgressIndicator(
-                  valueColor: AlwaysStoppedAnimation(
-                      StyleProvider.of(context).appColors.primaryContent),
-                )
-              : Text(
-                  label,
-                  style: TextStyle(
-                    color: StyleProvider.of(context).appColors.primaryContent,
+    return Container(
+      decoration: BoxDecoration(
+          boxShadow: [StyleProvider.of(context).shadow.mainShadow],
+          borderRadius: BorderRadius.circular(50)),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(50),
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          height: 50,
+          decoration: BoxDecoration(
+            color: color ?? StyleProvider.of(context).colors.primaryAccent,
+          ),
+          child: RawMaterialButton(
+            child: loading
+                ? CircularProgressIndicator(
+                    valueColor: AlwaysStoppedAnimation(
+                        StyleProvider.of(context).colors.primaryContent),
+                  )
+                : Stack(
+                    children: [
+                      if (icon != null) icon,
+                      Center(
+                        child: Text(
+                          label,
+                          style: TextStyle(
+                            color:
+                                StyleProvider.of(context).colors.primaryContent,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                ),
-          onPressed: loading ? null : onPressed,
+            onPressed: loading ? null : onPressed,
+          ),
         ),
       ),
     );
