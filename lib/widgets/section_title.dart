4528591cf442dc673bce1a94356d6bb13f8dba33
gonuit <kamilklyta341@gmail.com>
2019-10-28 09:02:34 +0100

    Add element slider widget

---
+++ lib/widgets/section_title.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:targowisko/utils/style_provider.dart';
+
+class SectionTitle extends StatelessWidget {
+  final String title;
+
+  SectionTitle({@required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      child: Text(
+        title,
+        style: StyleProvider.of(context).font.pacifico.copyWith(fontSize: 16),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/sliders/element_slider.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:targowisko/utils/style_provider.dart';
+import 'package:targowisko/widgets/section_title.dart';
+
+typedef SliderElementBuilder<T> = Widget Function(BuildContext context, T item);
+
+class ElementSlider<T> extends StatelessWidget {
+  final String title;
+  final double height;
+  final double cardWidth;
+  final List<T> items;
+  final SliderElementBuilder<T> elementBuilder;
+
+  ElementSlider({
+    @required this.title,
+    @required this.items,
+    @required this.elementBuilder,
+    this.cardWidth = 300,
+    this.height = 250,
+  })  : assert(items != null),
+        assert(elementBuilder != null);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(top: 15),
+      height: height,
+      width: MediaQuery.of(context).size.width,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          SectionTitle(
+            title: title,
+          ),
+          Expanded(
+            child: ListView.separated(
+              physics: BouncingScrollPhysics(),
+              padding: const EdgeInsets.all(15),
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (BuildContext context, int index) {
+                return SliderCard(
+                  width: cardWidth,
+                  child: elementBuilder(context, items[index]),
+                );
+              },
+              separatorBuilder: (BuildContext context, int index) =>
+                  SizedBox(width: 15),
+              itemCount: items.length,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class SliderCard extends StatelessWidget {
+  final double width;
+  final Widget child;
+
+  SliderCard({
+    @required this.width,
+    @required this.child,
+  })  : assert(width != null),
+        assert(child != null);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        width: width,
+        color: StyleProvider.of(context).colors.primaryAccent,
+        child: child,
+      ),
+    );
+  }
+}
