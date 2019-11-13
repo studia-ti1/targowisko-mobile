80efb1ecc226a459cf813b921b81f2d86ad063fa
gonuit <kamilklyta341@gmail.com>
2019-11-13 19:22:52 +0100

    Add api actions

--- lib/utils/api.dart
+++ lib/utils/api.dart
@@ -1,6 +1,38 @@
+import 'package:flutter/widgets.dart';
+import 'package:http/http.dart' as http;
+
 class Api {
   /// private constructor
   Api._();
 
   static String accesToken;
+
+  static Future<http.Response> fetchMarkets() async {
+    final result = await http.get(
+      'https://targowisko.herokuapp.com/api/v1/markets/',
+      headers: {
+        'access-token': Api.accesToken,
+      },
+    );
+
+    if (result.statusCode >= 300) throw ApiException(message: result.body);
+    return result;
+  }
+
+  static Future<http.Response> getAllEventsFromFb() async {
+    final result = await http.post(
+      'https://targowisko.herokuapp.com/api/v1/markets/fetch_from_api.json',
+      headers: {
+        'access-token': Api.accesToken,
+      },
+    );
+    if (result.statusCode >= 300) throw ApiException(message: result.body);
+    return result;
+  }
+}
+
+class ApiException implements Exception {
+  final String message;
+
+  ApiException({@required this.message});
 }
