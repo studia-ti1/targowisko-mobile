921af7691cb6eac3caef7bfdd5c62b0c4370b77a
gonuit <kamilklyta341@gmail.com>
2019-11-17 22:36:38 +0100

    Improve home_screen
    New API object

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,13 +1,9 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 import 'package:targowisko/models/market_model.dart';
 import 'package:targowisko/screens/home/widgets/market_card.dart';
 import 'package:targowisko/screens/home/widgets/scaffold_with_menu.dart';
 import 'package:targowisko/utils/alert.dart';
 import 'package:targowisko/utils/api.dart';
-import 'package:targowisko/utils/style_provider.dart';
 import 'package:targowisko/widgets/sliders/element_slider.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -29,55 +25,41 @@ class _HomeScreenState extends State<HomeScreen> {
     setState(() {
       _loading = true;
     });
-    http.Response result;
+    List<MarketModel> result;
     try {
-      result = await Api.fetchMarkets();
+      result = await Api.market.fetch();
     } on ApiException catch (err) {
       Alert.open(context, title: "Wystąpił błąd", content: err.message);
       return;
     }
     setState(() {
+      _markets = result;
       _loading = false;
-    });
-    final List jsonEventList = jsonDecode(result.body);
-    setState(() {
-      _markets = jsonEventList.map((dynamic market) {
-        return MarketModel.fromJson(market);
-      }).toList();
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return ScaffoldWithMenu(
-      builder: ({openMenu, closeMenu}) => _loading
-          ? Center(
-              child: CircularProgressIndicator(
-                valueColor: AlwaysStoppedAnimation(
-                    StyleProvider.of(context).colors.primaryAccent),
-              ),
-            )
-          : ListView(
-              children: <Widget>[
-                ElementSlider<MarketModel>(
-                  title: "Dostępne targi",
-                  elementBuilder: (context, market) =>
-                      MarketCard(market: market),
-                  items: _markets,
-                ),
-                FlatButton(
-                  child: Text("Sync"),
-                  onPressed: () async {
-                    setState(() {
-                      _loading = true;
-                    });
-                    Api.getAllEventsFromFb();
-
-                    _fetchMarkets();
-                  },
-                )
-              ],
-            ),
+      builder: ({openMenu, closeMenu}) => ListView(
+        children: <Widget>[
+          ElementSlider<MarketModel>(
+            title: "Dostępne targi",
+            elementBuilder: (context, market) => MarketCard(market: market),
+            items: _markets,
+          ),
+          ElementSlider<MarketModel>(
+            title: "Dostępne targi",
+            elementBuilder: (context, market) => MarketCard(market: market),
+            items: _markets,
+          ),
+          ElementSlider<MarketModel>(
+            title: "Dostępne targi",
+            elementBuilder: (context, market) => MarketCard(market: market),
+            items: _markets,
+          ),
+        ],
+      ),
     );
   }
 }
