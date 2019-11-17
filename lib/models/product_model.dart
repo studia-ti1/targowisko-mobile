1a2d40ba4e814c14ceea93e9753ea104c70ad51c
gonuit <kamilklyta341@gmail.com>
2019-11-17 22:35:15 +0100

    Add api action to product model
    update, add to market

--- lib/models/product_model.dart
+++ lib/models/product_model.dart
@@ -1,5 +1,12 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:targowisko/utils/api.dart';
+
 import 'market_model.dart';
 import 'owner_model.dart';
+
+import 'package:http/http.dart' as http;
 
 class ProductModel {
   final int id;
@@ -27,4 +34,73 @@ class ProductModel {
                 .toList()
             : null,
         productRatings = json["product_ratings"];
+
+  Future<ProductModel> update({
+    String name,
+    File picture,
+    int price,
+    int category,
+    String description, // ?
+  }) async {
+    assert(id != null);
+
+    final result = await http.patch(
+      'https://targowisko.herokuapp.com/api/v1/products/${this.id}',
+      body: <String, dynamic>{
+        "name": name,
+        "description": description,
+        "picture": picture,
+        "price": price,
+        "category": category,
+      },
+      headers: {
+        'access-token': Api.accesToken,
+      },
+    );
+
+    if (result.statusCode >= 300) throw ApiException(message: result.body);
+    return ProductModel.fromJson(json.decode(result.body));
+  }
+
+  Future<ProductModel> addToMarket(
+    int marketId,
+  ) async {
+    final url = Uri.https(
+      "targowisko.herokuapp.com/api/v1",
+      "markets/${marketId}/add_product",
+      <String, String>{
+        "product_id": this.id.toString(),
+      },
+    );
+    final result = await http.post(
+      url,
+      headers: {
+        'access-token': Api.accesToken,
+      },
+    );
+
+    if (result.statusCode >= 300) throw ApiException(message: result.body);
+    return ProductModel.fromJson(json.decode(result.body));
+  }
+
+  Future<ProductModel> removeFromMarket(
+    int marketId,
+  ) async {
+    final url = Uri.https(
+      "targowisko.herokuapp.com/api/v1",
+      "markets/${marketId}/remove_product",
+      <String, String>{
+        "product_id": this.id.toString(),
+      },
+    );
+    final result = await http.delete(
+      url,
+      headers: {
+        'access-token': Api.accesToken,
+      },
+    );
+
+    if (result.statusCode >= 300) throw ApiException(message: result.body);
+    return ProductModel.fromJson(json.decode(result.body));
+  }
 }
