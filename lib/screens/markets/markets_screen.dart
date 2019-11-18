9195a06c1eee4fd0e54d5ec2f07acb65c60dfa54
gonuit <kamilklyta341@gmail.com>
2019-11-18 14:11:39 +0100

    Add markets fetching into markets screen

--- lib/screens/markets/markets_screen.dart
+++ lib/screens/markets/markets_screen.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:targowisko/models/market_model.dart';
+import 'package:targowisko/utils/alert.dart';
+import 'package:targowisko/utils/api.dart';
 import 'package:targowisko/utils/style_provider.dart';
 import 'package:targowisko/widgets/extent_list_scaffold.dart';
+import 'package:targowisko/widgets/market_list_item.dart';
 
 class MarketsScreen extends StatefulWidget {
   @override
@@ -8,6 +12,31 @@ class MarketsScreen extends StatefulWidget {
 }
 
 class _MarketsScreenState extends State<MarketsScreen> {
+  List<MarketModel> _markets = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchMarkets();
+  }
+
+  Future<void> _fetchMarkets() async {
+    try {
+      final markets = await Api.market.fetch();
+      setState(() {
+        _markets = markets;
+      });
+    } on ApiException catch (err) {
+      Alert.open(
+        context,
+        title: "Wystąpił nieoczekiwany błąd",
+        content: err.message,
+        onConfirm: Navigator.of(context).pop,
+        confirmLabel: "Rozumiem",
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return ExtentListScaffold(
@@ -18,15 +47,12 @@ class _MarketsScreenState extends State<MarketsScreen> {
         height: 100,
       ),
       child: ListView.builder(
+        padding: const EdgeInsets.fromLTRB(15, 40, 15, 10),
         itemBuilder: (BuildContext context, int index) {
-          return Text(
-            index.toString(),
-            style: StyleProvider.of(context)
-                .font
-                .pacifico
-                .copyWith(color: Colors.black),
-          );
+          final market = _markets[index];
+          return MarketListItem(market: market);
         },
+        itemCount: _markets.length,
       ),
     );
   }
