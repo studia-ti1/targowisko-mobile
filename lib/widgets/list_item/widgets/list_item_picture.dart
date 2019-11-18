5c8be417463292c8c208b9159a29b4c2397d3e96
gonuit <kamilklyta341@gmail.com>
2019-11-18 23:50:24 +0100

    Add list_item_picture widget

---
+++ lib/widgets/list_item/widgets/list_item_picture.dart
@@ -0,0 +1,33 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:targowisko/utils/style_provider.dart';
+
+class ListItemPicture extends StatelessWidget {
+  final String imageUrl;
+
+  ListItemPicture({
+    @required this.imageUrl,
+  });
+
+  @override
+  Widget build(BuildContext context) => imageUrl != null
+      ? CachedNetworkImage(
+          fit: BoxFit.cover,
+          width: 110,
+          height: 110,
+          imageUrl: imageUrl,
+        )
+      : Container(
+          alignment: Alignment.center,
+          width: 110,
+          height: 110,
+          decoration: BoxDecoration(
+            color: StyleProvider.of(context).colors.primaryAccent,
+          ),
+          child: Image.asset(
+            StyleProvider.of(context).asset.appLogo,
+            width: 60,
+            height: 60,
+          ),
+        );
+}
