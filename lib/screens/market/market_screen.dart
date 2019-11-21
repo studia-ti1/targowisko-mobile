ca5f15ef33efeae610e6f2ed0d24e8f5ea39faa1
makz21 <maks.regula132@gmail.com>
2019-11-21 22:57:25 +0100

    Add missing content to market_screen widget

--- lib/screens/market/market_screen.dart
+++ lib/screens/market/market_screen.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:targowisko/screens/market/widgets/icon_section.dart';
 import 'package:targowisko/screens/market/widgets/organiser_section.dart';
+import 'package:targowisko/screens/market/widgets/section.dart';
+import 'package:targowisko/utils/style_provider.dart';
 import 'package:targowisko/widgets/extent_list_scaffold.dart';
 import 'package:targowisko/models/market_model.dart';
 import 'package:targowisko/widgets/extent_list_scaffold_image_nav_child.dart';
+import 'package:targowisko/widgets/raiting_coins.dart';
 
 class MarketScreenArgs {
   MarketModel market;
@@ -30,16 +34,54 @@ class _MarketScreenState extends State<MarketScreen> {
       liked: false,
       // TODO:
       onLikePress: () {},
-      title: market.name,
+      title: market?.name ?? "-",
       navChild: ExtentListScaffoldImageNavChild(
         imageUrl: market.imageUrl,
       ),
       child: ListView(
-        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 50),
+        padding: const EdgeInsets.fromLTRB(15, 30, 15, 15),
         children: <Widget>[
           OrganiserSection(
             market: market,
           ),
+          SizedBox(height: 15),
+          RaitingCoins(
+            size: 35,
+            value: market.averageRating,
+          ),
+          SizedBox(height: 15),
+          IconSection(
+            icon: Icons.calendar_today,
+            // TODO: missing data from /market endpoint
+            title: "14 Stycznia 2019",
+            content: "2h",
+          ),
+          SizedBox(height: 15),
+          IconSection(
+            icon: Icons.location_on,
+            title: market.place?.name ?? "Brak informacji o lokalizacji",
+            content: market.place?.location?.locationString ?? "-",
+          ),
+          SizedBox(height: 15),
+          Section(
+            title: "O wydarzeniu",
+            child: Text(
+              market.description,
+              style: StyleProvider.of(context).font.normal.copyWith(
+                    fontSize: 13,
+                    height: 1,
+                  ),
+            ),
+          ),
+          SizedBox(height: 15),
+          Section(
+            title: "Wystawcy",
+            // TODO:
+            child: Container(
+              height: 200,
+              color: Colors.red,
+            ),
+          )
         ],
       ),
     );
