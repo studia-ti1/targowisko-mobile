bb8da64196f2c62c8bef05200420a2ff24709c8f
FerdynandF <ferdynand.felsztukier@gmail.com>
2019-11-25 21:01:21 +0100

    improve rounded button

--- lib/widgets/buttons/rounded_button.dart
+++ lib/widgets/buttons/rounded_button.dart
@@ -3,25 +3,31 @@ import 'package:targowisko/utils/style_provider.dart';
 
 class RoundedButton extends StatelessWidget {
   final String title;
+  final double height;
   final VoidCallback onTap;
+  final BorderRadius borderRadius;
+  final Alignment contentAlignment;
 
   RoundedButton({
     @required this.title,
     @required this.onTap,
+    this.height,
+    this.borderRadius,
+    this.contentAlignment,
   }) : assert(title != null);
 
   @override
   Widget build(BuildContext context) {
     return ClipRRect(
-      borderRadius: BorderRadius.circular(5),
+      borderRadius: borderRadius ?? const BorderRadius.all(Radius.circular(5)),
       child: Material(
         color: StyleProvider.of(context).colors.primaryAccent,
         child: InkWell(
           onTap: onTap,
           child: Container(
-            alignment: Alignment.center,
+            alignment: contentAlignment ?? Alignment.center,
             padding: const EdgeInsets.symmetric(horizontal: 15),
-            height: 25,
+            height: height ?? 25,
             child: Text(
               title,
               style: StyleProvider.of(context).font.pacificoPrimary.copyWith(
