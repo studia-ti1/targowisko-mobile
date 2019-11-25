c17fbc0e0633c914c3a8bc098b5408c850a26b26
kamilklyta <ferdynand.felsztukier@gmail.com>
2019-11-25 15:15:51 +0100

    Improve section

--- lib/screens/market/widgets/section.dart
+++ lib/screens/market/widgets/section.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:targowisko/utils/style_provider.dart';
+import 'package:targowisko/widgets/buttons/rounded_button.dart';
 
 class Section extends StatelessWidget {
   final String title;
   final Widget child;
   final EdgeInsets titlePadding;
+  final VoidCallback onMorePress;
 
   Section({
     @required this.title,
-    this.titlePadding,
     @required this.child,
+    this.titlePadding,
+    this.onMorePress,
   }) : assert(title != null);
 
   @override
@@ -19,12 +22,31 @@ class Section extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.start,
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
+        Container(
+          margin: titlePadding ?? const EdgeInsets.only(bottom: 15),
+          height: 0.5,
+          decoration: BoxDecoration(
+            color: StyleProvider.of(context).colors.content.withOpacity(0.5),
+          ),
+        ),
         Padding(
           padding: titlePadding ?? const EdgeInsets.only(bottom: 15),
-          child: Text(
-            title,
-            style:
-                StyleProvider.of(context).font.pacifico.copyWith(fontSize: 16),
+          child: Row(
+            children: <Widget>[
+              Text(
+                title,
+                style: StyleProvider.of(context)
+                    .font
+                    .pacifico
+                    .copyWith(fontSize: 16),
+              ),
+              Spacer(),
+              if (onMorePress != null)
+                RoundedButton(
+                  onTap: onMorePress,
+                  title: "Więcej...",
+                ),
+            ],
           ),
         ),
         child,
