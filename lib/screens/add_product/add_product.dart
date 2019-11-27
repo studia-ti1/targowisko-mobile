5608c8f5b159c7d5323ddd53ac537fdd5c972905
gonuit <kamilklyta341@gmail.com>
2019-11-27 01:49:12 +0100

    Add categories to add_product screen

--- lib/screens/add_product/add_product.dart
+++ lib/screens/add_product/add_product.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:targowisko/models/product_model.dart';
 import 'package:targowisko/utils/alert.dart';
 import 'package:targowisko/utils/api.dart';
 import 'package:targowisko/utils/image_picker_util.dart';
@@ -8,6 +9,9 @@ import 'package:targowisko/utils/style_provider.dart';
 import 'package:targowisko/widgets/buttons/primary_button/primary_button.dart';
 import 'package:targowisko/widgets/input/input.dart';
 import 'package:targowisko/widgets/list_scaffold.dart';
+import 'package:targowisko/widgets/select_category_section/select_category_section.dart';
+
+import '../../routes.dart';
 
 class AddProductScreen extends StatefulWidget {
   @override
@@ -21,8 +25,13 @@ class _AddProductScreenState extends State<AddProductScreen> {
   final TextEditingController _price = TextEditingController();
   File _image;
   bool _hasImage = false;
+  ProductCategory _selectedCategory;
 
   void _addProduct() async {
+    if (_selectedCategory == null) {
+      Alert.open(context, title: "Brak danych", content: "Wybierz kategorię");
+      return;
+    }
     try {
       setState(() {
         _loading = true;
@@ -33,10 +42,17 @@ class _AddProductScreenState extends State<AddProductScreen> {
       final result = await Api.product.create(
         description: _description.text,
         name: _name.text,
-        category: 1,
+        category: _selectedCategory,
         picture: _hasImage ? _image : null,
         price: (price * 100).toInt(),
       );
+
+      _loading = false;
+
+      // TODO: Products storage?
+
+      Navigator.pushNamedAndRemoveUntil(
+          context, Routes.home, ModalRoute.withName(Routes.home));
     } on FormatException catch (err) {
       Alert.open(
         context,
@@ -56,6 +72,7 @@ class _AddProductScreenState extends State<AddProductScreen> {
         content: err.toString(),
       );
     } finally {
+      if (!_loading) return;
       setState(() {
         _loading = false;
       });
@@ -75,6 +92,7 @@ class _AddProductScreenState extends State<AddProductScreen> {
     );
 
     if (image == null) return;
+
     await Future<void>.delayed(const Duration(milliseconds: 300));
     setState(() {
       _hasImage = true;
@@ -92,82 +110,44 @@ class _AddProductScreenState extends State<AddProductScreen> {
           padding: const EdgeInsets.fromLTRB(15, 40, 15, 15),
           children: <Widget>[
             Center(
-              child: Container(
-                width: 200,
-                height: 200,
-                decoration: BoxDecoration(
-                  borderRadius: const BorderRadius.all(Radius.circular(20)),
-                  boxShadow: [StyleProvider.of(context).shadow.mainShadow],
-                  gradient: StyleProvider.of(context).gradient.cardGradient3,
-                ),
-                child: ClipRRect(
-                  borderRadius: const BorderRadius.all(Radius.circular(20)),
-                  child: Stack(
-                    children: <Widget>[
-                      Positioned.fill(
-                        child: AnimatedSwitcher(
-                          duration: const Duration(milliseconds: 300),
-                          child: _hasImage
-                              ? Container(
-                                  decoration: BoxDecoration(
-                                    image: DecorationImage(
-                                      image: FileImage(_image),
-                                      fit: BoxFit.cover,
-                                    ),
-                                  ),
-                                )
-                              : null,
-                        ),
-                      ),
-                      Positioned.fill(
-                        child: Material(
-                          color: Colors.transparent,
-                          child: InkWell(
-                            onTap: _pickOrRemoveImage,
-                            child: Padding(
-                              padding: const EdgeInsets.all(5),
-                              child: AnimatedAlign(
-                                curve: Curves.easeOut,
-                                alignment: _hasImage
-                                    ? Alignment.topLeft
-                                    : Alignment.center,
-                                duration: const Duration(milliseconds: 300),
-                                child: AnimatedSwitcher(
-                                  duration: const Duration(milliseconds: 300),
-                                  child: _hasImage
-                                      ? Icon(
-                                          Icons.delete_forever,
-                                          color: StyleProvider.of(context)
-                                              .colors
-                                              .primaryContent,
-                                          size: 40,
-                                        )
-                                      : Icon(
-                                          Icons.add_photo_alternate,
-                                          color: StyleProvider.of(context)
-                                              .colors
-                                              .primaryContent,
-                                          size: 40,
-                                        ),
-                                ),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
+              child: PhotoAddContainerWidget(
+                hasImage: _hasImage,
+                image: _image,
+                onTap: _pickOrRemoveImage,
               ),
+            ),
+            SizedBox(height: 30),
+            Text(
+              "Kategoria",
+              style: StyleProvider.of(context)
+                  .font
+                  .pacifico
+                  .copyWith(fontSize: 17),
+            ),
+            SizedBox(height: 5),
+            SelectCateogryWidget(
+              onChange: (ProductCategory category) {
+                setState(() {
+                  _selectedCategory = category;
+                });
+              },
+              onRemove: () {
+                setState(() {
+                  _selectedCategory = null;
+                });
+              },
+              selected: _selectedCategory,
             ),
             SizedBox(height: 25),
             Input(
+              enabled: !_loading,
               controller: _name,
               title: "Nazwa produktu",
               withBorder: true,
             ),
             const SizedBox(height: 10),
             Input(
+              enabled: !_loading,
               title: "Opis",
               controller: _description,
               lines: 5,
@@ -176,6 +156,7 @@ class _AddProductScreenState extends State<AddProductScreen> {
             const SizedBox(height: 10),
             Input(
               title: "Cena",
+              enabled: !_loading,
               controller: _price,
               keyboardType: const TextInputType.numberWithOptions(
                 decimal: false,
@@ -188,6 +169,92 @@ class _AddProductScreenState extends State<AddProductScreen> {
               label: "Dodaj produkt",
               loading: _loading,
               onPressed: _loading ? null : _addProduct,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class PhotoAddContainerWidget extends StatelessWidget {
+  const PhotoAddContainerWidget({
+    Key key,
+    @required bool hasImage,
+    @required File image,
+    @required VoidCallback onTap,
+  })  : _hasImage = hasImage,
+        _image = image,
+        _onTap = onTap,
+        super(key: key);
+
+  final bool _hasImage;
+  final File _image;
+  final VoidCallback _onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 200,
+      height: 200,
+      decoration: BoxDecoration(
+        borderRadius: const BorderRadius.all(Radius.circular(20)),
+        boxShadow: [StyleProvider.of(context).shadow.mainShadow],
+        gradient: StyleProvider.of(context).gradient.cardGradient3,
+      ),
+      child: ClipRRect(
+        borderRadius: const BorderRadius.all(Radius.circular(20)),
+        child: Stack(
+          children: <Widget>[
+            Positioned.fill(
+              child: AnimatedSwitcher(
+                duration: const Duration(milliseconds: 300),
+                child: _hasImage
+                    ? Container(
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: FileImage(_image),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      )
+                    : null,
+              ),
+            ),
+            Positioned.fill(
+              child: Material(
+                color: Colors.transparent,
+                child: InkWell(
+                  onTap: _onTap,
+                  child: Padding(
+                    padding: const EdgeInsets.all(5),
+                    child: AnimatedAlign(
+                      curve: Curves.easeOut,
+                      alignment:
+                          _hasImage ? Alignment.topLeft : Alignment.center,
+                      duration: const Duration(milliseconds: 300),
+                      child: AnimatedSwitcher(
+                        duration: const Duration(milliseconds: 300),
+                        child: _hasImage
+                            ? Icon(
+                                Icons.delete_forever,
+                                color: StyleProvider.of(context)
+                                    .colors
+                                    .primaryContent,
+                                size: 40,
+                              )
+                            : Icon(
+                                Icons.add_photo_alternate,
+                                color: StyleProvider.of(context)
+                                    .colors
+                                    .primaryContent,
+                                size: 40,
+                              ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
             ),
           ],
         ),
